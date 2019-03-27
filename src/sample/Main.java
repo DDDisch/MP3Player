2d3a5eb242cafcac051203cfe114aff5499ccf15
michi7801 <michael.kaltenbrunner2@sz-ybbs.ac.at>
2019-03-27 15:57:15 +0100

    Deleted unnesscesary classes

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -10,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Group root = new Group();
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
