854d578257cb250336ea696a7a3c86f01f636dff
DDDisch <philipp.dvorak@sz-ybbs.ac.at>
2019-04-04 11:46:50 +0200

    Added Main Files after reverting Changes

---
+++ src/sample/Main.java
@@ -0,0 +1,117 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import last.fm.lastAPI;
+import FileCommunicator.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.concurrent.TimeoutException;
+
+public class Main extends Application {
+
+    private lastAPI api = new lastAPI();
+    private Button moreInfo;
+    private TextField author;
+    private FileCommunicator apiSettings;
+    private String tmpS="";
+    GridPane root;
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            apiSettings = new FileCommunicator(new File("./res/settings/api.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+
+        Button stopAndPlay = new Button("||");
+        Button forward = new Button("<");
+        Button backward = new Button(">");
+
+        primaryStage.setTitle("Last.fm API");
+        primaryStage.setScene(new Scene(root, 300, 275));
+
+        moreInfo = new Button("More Information");
+
+        if(apiSettings.readFile().contains("Author:")) {
+            String tmp = apiSettings.readFile();
+            ArrayList<String> tmpA= new ArrayList<>(Arrays.asList(tmp.split("(?<= )")));
+            tmpA.remove(0);
+
+            for (String s : tmpA) tmpS += s;
+
+            author = new TextField("" + tmpS);
+        }
+        else {
+            author = new TextField("U2");
+        }
+
+        addToRoot(author,0,0);
+        addToRoot(moreInfo,0,1);
+        addToRoot(backward,1,3);
+        addToRoot(stopAndPlay,2,3);
+        addToRoot(forward,3,3);
+
+
+        //GridPane.setConstraints(moreInfo, 0, 0);
+        //GridPane.setConstraints(author, 0, 1);
+        //root.getChildren().add(moreInfo);
+        //root.getChildren().add(author);
+
+        primaryStage.show();
+        primaryStage.titleProperty().bind(author.textProperty());
+        addListener();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                if(!author.getText().equals("")) {
+                    apiSettings.writeLine("Author: " + author.getText());
+                } else {
+                    apiSettings.writeLine("None");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }));
+    }
+
+    private void addToRoot(Node Element, int col, int row) {
+        GridPane.setConstraints(Element, col, row);
+        GridPane.setHalignment(Element, HPos.CENTER);
+        root.getChildren().add(Element);
+    }
+
+    private void addListener() {
+        moreInfo.setOnAction(e -> {
+            String tmp = "";
+            try {
+                tmp = api.authorAPI(author.getText());
+            } catch (TimeoutException | InterruptedException e1) {
+                e1.printStackTrace();
+            }
+
+            windows.authorPopUp.authorWindow(tmp);
+        });
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/windows/authorPopUp.java
@@ -0,0 +1,70 @@
+package windows;
+
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class authorPopUp {
+    private static ArrayList<String> apiArr;
+
+    public static void authorWindow(String api) {
+        createArr(api);
+
+        Stage stage = new Stage();
+        stage.initStyle(StageStyle.UNDECORATED);
+        GridPane root = new GridPane();
+        ImageView iv = new ImageView();
+        if(!filterArray(4,"img").isEmpty()) {
+            iv.setImage(new Image(filterArray(4, "img")));
+        }
+        GridPane.setConstraints(iv, 0,0);
+        root.getChildren().add(iv);
+
+        stage.setScene(new Scene(root, 400, 400));
+        stage.show();
+        stage.setTitle(filterArray(0, "title"));
+
+        addItem(root, new Label(filterArray(0, "title")));
+
+        stage.addEventFilter(KeyEvent.KEY_PRESSED, e-> {
+            if(e.getCode() == KeyCode.ESCAPE) {
+                stage.close();
+            }
+        });
+    }
+
+    private static void addItem(GridPane root, Node node) {
+        GridPane.setConstraints(node, 0,root.getChildren().size());
+        root.getChildren().add(node);
+    }
+
+    private static void createArr(String api) {
+        apiArr = new ArrayList<>(Arrays.asList(api.split(",")));
+
+        //Delete MBID entry, because not every Artist has an MBID
+        //Reason: To prevent different indexes after the MBID
+        if(apiArr.get(1).contains("mbid"))
+            apiArr.remove(1);
+    }
+
+    //Call Filter Array with every Index except Null, because the spliting of the API return String does not split the first with the last items
+    private static String filterArray(int index, String mode) {
+        if(mode.equals("normal"))
+            return new ArrayList<>(Arrays.asList(apiArr.get(index).split(":"))).get(1).replace("\"", "");
+        if(mode.equals("img"))
+            return new ArrayList<>(Arrays.asList(apiArr.get(index).split(":",2))).get(1).replace("\"", "");
+        if(mode.equals("title"))
+            return new ArrayList<>(Arrays.asList(apiArr.get(0).split(":"))).get(2).replace("\"", "");
+        return "";
+    }
+}
